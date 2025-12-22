1953960549b3b701da4bd8535d6a65928fef30ca
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-12-22 21:29:35 +0530

    Added the logout route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -61,4 +61,14 @@ authRouter.post("/auth/login",async(req,res) => {
     }
 })
 
+//Logout Route
+authRouter.post("/auth/logout",async(req,res) => {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message: "User Logged out successfully"})
+    } catch (error) {
+        return res.status(500).json({message: "Internal Server Error"})
+    }
+})
+
 module.exports = authRouter
